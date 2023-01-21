522c4380cdc03115bbc4938cd8f6afd00dfd0d21
gtim04 <gerald.timtiman@gmail.com>
2023-01-21 16:20:26 +0800

    ending vue basic tutorials

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ const app = Vue.createApp({
     // template: '<h2>I am the template</h2>'
     data() {
         return {
+            url: 'http://www.thenetninja.co.uk',
             showBooks: true,
             title: 'The Final Empire',
             author: 'Tim Timtiman',
-            age: '29'
+            age: '29',
+            x: 0,
+            y: 0,
+            books: [
+                { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/book1.png', isFav:true},
+                { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/book2.jpg', isFav:false},
+                { title: 'the final empire', author: 'brandon sanderson', img: 'assets/book3.jpg', isFav:true}
+            ]
         }
     },
     methods: {
@@ -17,6 +25,27 @@ const app = Vue.createApp({
         },
         toggleShowBooks(){
             this.showBooks = !this.showBooks
+        },
+        handleEvent(e, arg)
+        {
+            console.log(e.type)
+            if(arg){
+                console.log(arg)
+            }
+        },
+        handleMousemove(e){
+            this.x = e.offsetX
+            this.y = e.offsetY
+        },
+        toggleFav(book)
+        {
+            book.isFav = !book.isFav
+            // this.books[key].isFav = !this.books[key].isFav
+        },
+    },
+    computed: {
+        filteredBooks() {
+            return this.books.filter((book) => book.isFav)
         }
     }
 })
